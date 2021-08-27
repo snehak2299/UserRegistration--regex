ce5efa6f4487a435363ebf5ea4fc1eaea0ec8f00
snehak2299 <skanase2299@gmail.com>
2021-08-27 21:57:32 +0530

    [sneha]:first letter of surname should be capital and more than 3 alphabet

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,14 +6,14 @@ public class UserRegistration{
 	public static void main(String[] args) {
 		String regex = "^[A-Z][a-z]{3,}";
 		Scanner sc = new Scanner (System.in);
-		System.out.println("enter first name");
-		String name = sc.nextLine();
-		if(name.matches(regex)) {
-			System.out.println("name is valid");
+		System.out.println("enter last name");
+		String lastName = sc.nextLine();
+		if(lastName.matches(regex)) {
+			System.out.println("last name is valid");
 		}
 		else 
 		{
-			System.out.println("name is not valid");
+			System.out.println("last name is not valid");
 		}
 	
 	}
